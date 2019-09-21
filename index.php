3c1c212f3fe28db0e8370607acb7acf6bf12b20d
boyuan12 <50649079+boyuan12@users.noreply.github.com>
2019-09-21 14:43:32 -0700

    Create index.php
    create Message form in index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <main>
+      <p>SEND E-MAIL</p>
+      <form class="contact-form" action="boyuan12.github.io/DevWithMe/contactform.php method="post">
+        <input type="text" name="name" placeholder="Full name">
+        <input type="text" name="mail" placeholder="Your e-mail">
+        <input type="text" name="subject" placeholder="Subject">
+        <textarea name="message" placeholder="Message"></textarea>
+        <button type="submit" name="submit">SEND MAIL
+    </main>
+  </body>
